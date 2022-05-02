80b21f91d850acc06f6752bb148925a92edaa29e
Murilo Rocha <software@murilorocha.com.br>
2022-05-02 20:13:07 -0300

    Teste de uso do comentário de múltiplas linhas para deixar de executar várias linhas de código em Java.

---
+++ src/TesteComentarioMultiploCodigo.java
@@ -0,0 +1,14 @@
+public class TesteComentarioMultiploCodigo {
+
+    public static void main(String[] args) {
+        /*
+        String mensagem = "Essa é a mensagem que não deve ser exibida";
+
+        System.out.println(mensagem);
+        */
+        String mensagemCorreta = "Essa é a mensagem que deve ser exibida";
+
+        System.out.println(mensagemCorreta);
+
+    }
+}
